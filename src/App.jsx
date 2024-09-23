bafb6319385565d47ee4dde8f738e876f29ae6e2
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2024-09-23 22:53:40 +0530

    setup tailwind-flowbite for image cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import ImageCard from './cards/Cards'
 import Navbar from './navbar/navbar'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
     {/* <h1 className='text-5xl text-center bg-green-200 p-5 font-bold'>La - Galleria</h1> */}
     <Navbar/>
+    <ImageCard/>
     </>
   )
 }
