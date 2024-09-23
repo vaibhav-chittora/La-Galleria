afd1785a1bcf4a47273b21a37842c99792ab7817
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2024-09-23 22:09:17 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='text-5xl text-center bg-green-200 p-5 font-bold'>La - Galleria</h1>
+    </>
+  )
+}
+
+export default App
