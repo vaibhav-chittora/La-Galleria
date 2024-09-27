afb1ac0e624217f8c673894ed5bf4ae41925cb1b
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2024-09-27 20:20:16 +0530

    fetched the data and added Cards layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
+import { useEffect } from 'react'
 import './App.css'
 import ImageCard from './cards/Cards'
 import Navbar from './navbar/navbar'
 
 function App() {
+  useEffect(() => {
+    async function fetchImages() {
+      try {
+        const url = 'https://pixabay.com/api/?key=46184021-ab03fabc0d1c124ec59d8a3e9&q=yellow+flowers&image_type=photo&pretty=true'
+        const response = await fetch(url)
+        const data = await response.json()
+        console.log(data);
+        // console.log("largeImageURL",data.hits[2].largeImageURL);
+        // console.log("previewURL ",data.hits[2].previewURL);
+        console.log("webformatURL ",data.hits[2].webformatURL);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchImages()
+  }, [])
 
   return (
     <>
-    {/* <h1 className='text-5xl text-center bg-green-200 p-5 font-bold'>La - Galleria</h1> */}
-    <Navbar/>
-    <ImageCard/>
+      {/* <h1 className='text-5xl text-center bg-green-200 p-5 font-bold'>La - Galleria</h1> */}
+      <Navbar />
+      <ImageCard />
     </>
   )
 }
